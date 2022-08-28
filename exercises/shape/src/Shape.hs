ff33e69e8112372145b8263b3713d24a3d3b62d5
sencina <santiago.encina@ing.austral.edu.ar>
2022-08-28 15:17:21 -0300

    Shape

--- exercises/shape/src/Shape.hs
+++ exercises/shape/src/Shape.hs
@@ -1,4 +1,4 @@
-module Shape where
+module Shape (Point(..),Circle(..),Rectangle(..),point,origin,rectangle,base,height,shift,circle,surface,Surface(..)) where
 
 data Point = Point { x::Double, y:: Double} deriving (Eq, Show)
 
@@ -8,25 +8,31 @@ data Rectangle = Rectangle Point Point deriving (Eq, Show)
 
 -- A point from a tuple Pair
 point::(Double, Double) -> Point
-point = error "Implement it"
+point (a, b) = Point{x=a,y=b}
 
 -- The origin
 origin::Point
-origin = error "Implement it"
+origin = point (0,0)
 
 -- Rectangle from a Tuple where (x0 y0) == origin
 rectangle::(Double, Double) -> Rectangle
-rectangle = error "Implement it" 
+rectangle (a,b)= Rectangle Point{x=0, y=0} Point{x=a,y=b}
 
 base::Rectangle -> Double
-base = error "Implement it"
+base (Rectangle pointA pointB) = (x pointB) - (x pointA)
 
 height::Rectangle -> Double
-height = error "Implement it"
+height (Rectangle pointA pointB) = (y pointB) - (y pointA)
+
+pointA::Rectangle -> Point
+pointA (Rectangle pointA _)= pointA
+
+pointB::Rectangle -> Point
+pointB (Rectangle _ pointB)= pointB
 
 -- Circle from radius
 circle::Double -> Circle
-circle = error "Implement it" 
+circle radius = Circle (point (0,0)) radius
 
 -- Clase Shift
 
@@ -34,13 +40,21 @@ class Shift a where
    shift::a -> (Double, Double) -> a
    
 instance Shift Point where
-   shift  = error "Implement it"
+   shift aPoint (a,b) = Point{x=((x aPoint) + a),y=((y aPoint)+ b)}
    
 instance Shift Rectangle where
-   shift  = error "Implement it"
+   shift rectangle (a,b)= Rectangle (point (a+(x (pointA rectangle)),b+(y (pointA rectangle)))) (point (a+(x (pointB rectangle)),b+(y (pointB rectangle))))
    
 instance Shift Circle where
-   shift  = error "Implement it"
+   shift (Circle _ radius) (a,b) = Circle (point (a,b)) radius
    
 -- Define the Surface class
-   
+
+class Surface a where
+  surface:: a -> Double
+
+instance Surface Rectangle where
+  surface rectangle = (base rectangle) * (height rectangle)
+
+instance Surface Circle where
+  surface (Circle _ radius) = pi * (radius^2)
