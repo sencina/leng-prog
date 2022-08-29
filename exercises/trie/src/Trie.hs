4908a0bf30d33cc1e8af3a23b1ac611e17589f14
sencina <santiago.encina@ing.austral.edu.ar>
2022-08-29 17:05:10 -0300

    only missing toList

--- exercises/trie/src/Trie.hs
+++ exercises/trie/src/Trie.hs
@@ -12,7 +12,6 @@ right::Trie a -> Trie a
 right (Leaf _) = error "Is Leaf"
 right (_:-:a) = a
 
-  
 find::Bits -> Trie a -> a
 find (x:xs) (Leaf a) = error "Not found"
 find [] (Leaf a) = a
@@ -21,7 +20,14 @@ find (x:xs) (l :-: r)
     | x == T = find (xs) r
 
 decode::Bits -> Trie Char -> String
-decode = error "Define it"
+decode bits trie = decodeAux bits trie trie
+
+decodeAux:: Bits -> Trie Char -> Trie Char -> String
+decodeAux [] (Leaf a) trie = [a]
+decodeAux bits (Leaf a) trie = a : decodeAux bits trie trie
+decodeAux (x:xs) (l :-: r) trie
+    | x == F = decodeAux (xs) l trie
+    | x == T = decodeAux (xs) r trie
 
 toList::Trie a -> [(a, Bits)]
 toList = error "Define it"
