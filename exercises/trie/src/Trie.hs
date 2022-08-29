20c21489ba4e776944fc6526b2d52e32b8366c05
sencina <santiago.encina@ing.austral.edu.ar>
2022-08-29 16:25:24 -0300

    Trie faltan los ultimos dos ejercicios

--- exercises/trie/src/Trie.hs
+++ exercises/trie/src/Trie.hs
@@ -2,16 +2,23 @@ module Trie  (Trie(..), left, right, find, decode, toList) where
 
 import Bit
   
-data Trie a = Complete -- Add the Type definition deriving (Eq, Show)
-            
+data Trie a  = Leaf a | (Trie a) :-: (Trie a) deriving (Eq,Show)
+
 left::Trie a -> Trie a
-left = error "Define it"
+left (Leaf _) = error "Is Leaf"
+left (a:-:_) = a
 
 right::Trie a -> Trie a
-left = error "Define it"
+right (Leaf _) = error "Is Leaf"
+right (_:-:a) = a
+
   
 find::Bits -> Trie a -> a
-find = error "Define it"
+find (x:xs) (Leaf a) = error "Not found"
+find [] (Leaf a) = a
+find (x:xs) (l :-: r)
+    | x == F = find (xs) l
+    | x == T = find (xs) r
 
 decode::Bits -> Trie Char -> String
 decode = error "Define it"
