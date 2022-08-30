6d6c10b884245eef51c0a8aa327b2241d9e3c88e
sencina <santiago.encina@ing.austral.edu.ar>
2022-08-30 18:45:49 -0300

    freqMap

--- exercises/frequencies/src/Frequencies.hs
+++ exercises/frequencies/src/Frequencies.hs
@@ -1,8 +1,9 @@
-module Frequencies  (Frequency, frequencyMap, frequencies, insert, insertionSort) where
+module Frequencies  (Frequency, frequencyMap, frequencies, Frequencies.insert, insertionSort) where
 
 import Data.Map(Map)
 import qualified Data.Map as Map
 import Data.Tuple(swap)
+import Data.Map.Strict
 
 type Frequency = (Int, Char)
 
@@ -10,7 +11,15 @@ frequencies::String -> [Frequency]
 frequencies = error "Implement it"
 
 frequencyMap::(Ord a) => [a] -> Map a Int
-frequencyMap = error "Implement it"
+frequencyMap list = frequencyMapAux list Map.empty
+
+frequencyMapAux:: (Ord a) => [a] -> Map a Int -> Map a Int
+frequencyMapAux [] map = map
+frequencyMapAux (x:xs) map = frequencyMapAux xs (Data.Map.Strict.insertWith (+) x 1 map)
 
 insert::(Ord a) => a -> [a] -> [a]
-insert = error "Implement it"
+insert a [] = [a]
+insert a (x:xs)= if a <= x then a:x:xs else x: Frequencies.insert a xs
+
+insertionSort:: (Ord a) => [a] -> [a]
+insertionSort (x:xs) = Frequencies.insert x (insertionSort xs)
