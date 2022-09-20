1396032a55eacf98b202831045aebd7e7c46d69e
sencina <santiago.encina@ing.austral.edu.ar>
2022-09-20 18:03:53 -0300

    collatz complete

--- rust-exercises/collatz-conjecture/src/lib.rs
+++ rust-exercises/collatz-conjecture/src/lib.rs
@@ -1,4 +1,18 @@
 pub fn collatz(mut n: u64) -> Option<u64> {
-  None
+
+    let mut count = 0;
+
+    while n != 1 {
+        if (n % 2) == 0 {
+            n = n/2;
+        }
+        else{
+            n = 3 * n + 1
+        }
+        count+= 1;
+    }
+
+    return Some(count);
+    
 }
 
