56616891c9ad4d710d90c1c8a7e7a126b5ab3431
sencina <santiago.encina@ing.austral.edu.ar>
2022-10-07 10:16:12 -0300

    Sum of multiples complete

--- rust-exercises/sum-of-multiples/src/lib.rs
+++ rust-exercises/sum-of-multiples/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    0
+
+    let mut sum = 0;
+
+    for i in 0..limit {
+        for j in factors {
+            if *j != 0 && i % *j == 0 {
+                sum += i;
+                break;
+            }
+        }
+    }
+
+    return sum
+
 }
