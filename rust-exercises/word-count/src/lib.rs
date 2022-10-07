59ea20ca579ae93e29264d60abb94e97b2694512
sencina <santiago.encina@ing.austral.edu.ar>
2022-10-07 10:22:41 -0300

    Word count complete

--- rust-exercises/word-count/src/lib.rs
+++ rust-exercises/word-count/src/lib.rs
@@ -2,5 +2,15 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
-  panic!("Implement me!")
+
+    let mut map = HashMap::new();
+
+    for word in phrase.split_whitespace() {
+        let word = word.to_lowercase();
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    map
+
 }
