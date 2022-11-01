612fbc59bca6d5e6ab87a62b0275795033ad15cd
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2022-11-01 17:56:27 -0300

    complete

--- java-exercises/protein-translation/src/main/java/ProteinTranslator.java
+++ java-exercises/protein-translation/src/main/java/ProteinTranslator.java
@@ -4,10 +4,25 @@ import java.util.stream.*;
 class ProteinTranslator {
 
     List<String> translate(String rnaSequence) {
-       throw new IllegalArgumentException("Implement me!");
+        return Stream.iterate(0, i -> i + 3)
+                .limit(rnaSequence.length() / 3)
+                .map(i -> rnaSequence.substring(i, i + 3))
+                .map(ProteinTranslator::proteinFor)
+                .takeWhile(s -> !s.equals("STOP"))
+                .collect(Collectors.toList());
     }
 
     static String proteinFor(String codon) {
-        throw new IllegalArgumentException("Implement me!");
+        return switch (codon){
+            case "AUG" -> "Methionine";
+            case "UUU", "UUC" -> "Phenylalanine";
+            case "UUA", "UUG" -> "Leucine";
+            case "UCU", "UCC", "UCA", "UCG" -> "Serine";
+            case "UAU", "UAC" -> "Tyrosine";
+            case "UGU", "UGC" -> "Cysteine";
+            case "UGG" -> "Tryptophan";
+            case "UAA", "UAG", "UGA" -> "STOP";
+            default -> throw new IllegalArgumentException("Invalid codon");
+        };
     }
 }
